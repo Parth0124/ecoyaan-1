4328cd3f6cc1d85158230dc9deddd41fdbb0c7e4
Arya Sali <123355820+Arya2422@users.noreply.github.com>
2024-04-21 20:05:03 +0530

    Update Hero.js

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import './Hero.css';
+import './Hero.css'; // Importing CSS file for styling
+import Products from '../../Components/Products/Products'
 
-import Products from '../Products/Products';
-
-
-function Hero() {
+const Hero = () => {
   return (
-    <div className='main'>
-      <div style={{ textAlign: 'center' }}>
-        <div className='main_wrapper'>
-          <div className='main_container' style={{
-            display: 'inline-block',
-            backgroundColor: '#f0f0f0', // Example background color
-            padding: '20px',
-            borderRadius: '10px',
-            maxWidth: '80%', // Adjust maximum width as needed
-            margin: '0 auto' // Center the container horizontally
-          }}>
-            <p className='main_animatedText' style={{
-              fontSize: '20px', // Adjust font size as needed
-              margin: '0' // Remove default margins
-            }}>
-              Carefully Curated Eco-Friendly <span style={{ fontWeight: 'bold' }}>Products</span>
-            </p>
-          </div>
-        </div>
-      </div>
-     
-     <Products/>
-  
+    <div className="heading-container">
+      <h6 className="responsive-heading">
+        Carefully Curated Eco-Friendly Products{' '}
+        <span className="last-word">Products</span>
+      </h6>
+      <Products/>
     </div>
   );
-}
+};
 
 export default Hero;
